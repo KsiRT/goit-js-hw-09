1c70bd763f5bdeb1f0b3ffb1f2d50e47bd4e1bd8
KsiRT <130671776+KsiRT@users.noreply.github.com>
2023-07-09 22:27:15 +0000

    Deploying to gh-pages from @ KsiRT/goit-js-hw-09@f1bddf5825e54ab9a7b1ac56901ac82a8ea6ee59 🚀

---
+++ 01-color-switcher.659341cd.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");t.addEventListener("click",(t=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`})),o.addEventListener("click",(t=>{}));
+//# sourceMappingURL=01-color-switcher.659341cd.js.map
---
+++ 01-color-switcher.6c7c0dea.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");t.addEventListener("click",(function(t){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))})),o.addEventListener("click",(function(t){}))}();
+//# sourceMappingURL=01-color-switcher.6c7c0dea.js.map
