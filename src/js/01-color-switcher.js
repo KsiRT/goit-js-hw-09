f1bddf5825e54ab9a7b1ac56901ac82a8ea6ee59
KsiRT <130671776+KsiRT@users.noreply.github.com>
2023-07-10 01:26:25 +0300

    initCommit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const startButton = document.querySelector('button[data-start]');
+// console.log(startButton);
+const stopButton = document.querySelector('button[data-stop]');
+// console.log(stopButton);
 
+const onStartClick = evt => {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+const onStopClick = evt => {};
+
+startButton.addEventListener('click', onStartClick);
+stopButton.addEventListener('click', onStopClick);
